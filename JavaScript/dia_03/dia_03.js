e6a7a77cb0149622818ae51008b047b230460b91
Ronaldo Junior <dev.ronaldojunior@gmail.com>
2024-03-03 11:20:36 -0300

    Dia 03
    Dia 3 = Crie uma lista com alguns itens e imprima cada um deles.

---
+++ JavaScript/dia_03/dia_03.js
@@ -0,0 +1,7 @@
+// Dia 3 = Crie uma lista com alguns itens e imprima cada um deles.
+
+let lista = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+
+for(item in lista) {
+    alert(lista[item])
+}
---
+++ Python/dia_03.py
@@ -0,0 +1,9 @@
+# Dia 3 = Crie uma lista com alguns itens e imprima cada um deles.
+
+from time import sleep
+
+lista = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+
+for item in lista:
+    sleep(0.5)
+    print(item)
